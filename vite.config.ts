8c33864839487ec8124f8cabab262fa65faf0b7c
adekang <adekang@163.com>
2023-04-05 12:40:32 +0800

    add: pina vue-router auto import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,49 @@
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      reactivityTransform: true,
+    }),
+    AutoImport({
+      imports: [
+        'vue',
+        'vue/macros',
+        'vue-router',
+        'vue-i18n',
+        '@vueuse/core',
+        'pinia',
+        {
+          'naive-ui': [
+            'useDialog',
+            'useMessage',
+            'useNotification',
+            'useLoadingBar',
+          ],
+        },
+      ],
+      // 生成到的地址
+      dts: 'types/auto-imports.d.ts',
+      // 配置本地需要自动导入的库
+      dirs: [
+        // pinia状态管理目录
+        'src/stores',
+        // 自定义组合式api目录
+        'src/composables',
+      ],
+    }),
+    Components({
+      // 导入naiveui的配置项目
+      resolvers: [NaiveUiResolver()],
+      // 生成类型的地址
+      dts: 'types/components.d.ts',
+      dirs: [
+        'src/components',
+      ],
+    }),
+  ],
 })
