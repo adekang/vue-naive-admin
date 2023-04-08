cbc39c18834e8aa86e2cf8d6c61da06c4bbc689b
adekang <adekang@163.com>
2023-04-08 16:19:22 +0800

    feat: 全局基本布局配置

---
+++ src/layouts/base-layout/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    全局布局文件
+    <router-view />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/routes/static-routes.ts
+++ src/routes/static-routes.ts
@@ -1,18 +1,27 @@
 import type { RouteRecordRaw } from 'vue-router'
+import { Layout } from '@/layouts'
 
 const staticRoutes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: () => import('@/pages/index.vue'),
     name: 'index',
-    meta: {
-      title: 'Home',
-    },
-  },
-  {
-    path: '/workspace',
-    component: () => import('@/pages/workspace/index.vue'),
-    name: 'workspace',
+    component: Layout,
+    redirect: '/home',
+    children: [
+      {
+        path: '/home',
+        name: 'Home',
+        component: () => import('@/pages/index.vue'),
+        meta: {
+          title: 'Home',
+        },
+      },
+      {
+        path: '/workspace',
+        name: 'Workspace',
+        component: () => import('@/pages/workspace/index.vue'),
+      },
+    ],
   },
 ]
 
