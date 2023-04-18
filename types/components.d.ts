a3d224e9b95252cfac3b703295d252536957682c
adekang <adekang@163.com>
2023-04-18 18:33:13 +0800

    core: add baseUrl

--- types/components.d.ts
+++ types/components.d.ts
@@ -14,7 +14,6 @@ declare module '@vue/runtime-core' {
     NAvatar: typeof import('naive-ui')['NAvatar']
     NBadge: typeof import('naive-ui')['NBadge']
     NButton: typeof import('naive-ui')['NButton']
-    NCard: typeof import('naive-ui')['NCard']
     NCheckbox: typeof import('naive-ui')['NCheckbox']
     NConfigProvider: typeof import('naive-ui')['NConfigProvider']
     NDialogProvider: typeof import('naive-ui')['NDialogProvider']
