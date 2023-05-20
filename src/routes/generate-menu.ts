0f44529bfc276bc6e6ef3de81c46219f85addb05
adekang <adekang@163.com>
2023-05-20 15:16:19 +0800

    feat: 侧边栏路由跳转功能

--- src/routes/generate-menu.ts
+++ src/routes/generate-menu.ts
@@ -1,12 +1,13 @@
 import type { MenuOption } from 'naive-ui'
 import type { RouteRecordRaw } from 'vue-router'
+import SideTitle from '@/layouts/side-menu/side-title.vue'
 
 export const generateMenu = (routes: RouteRecordRaw[]) => {
   const menus: MenuOption[] = []
   for (const route of routes) {
     const currentMenu: MenuOption = {
       key: route.path,
-      label: route.meta?.title,
+      label: () => h(SideTitle, { route }),
     }
 
     if (route.children && route.children.length > 0)
