c77949810c50ffcb623e44bfc042d55316a2a4a4
adekang <adekang@163.com>
2023-05-20 15:29:10 +0800

    feat: 侧边栏路由图标

---
+++ src/layouts/side-menu/side-icon.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import * as icons from '@vicons/antd'
+// import icons from './icons'
+
+defineProps<{
+  icon: string
+}>()
+</script>
+
+<template>
+  <n-icon :component="icons[icon]" />
+</template>
+
+<style scoped>
+
+</style>
--- src/routes/generate-menu.ts
+++ src/routes/generate-menu.ts
@@ -1,6 +1,7 @@
 import type { MenuOption } from 'naive-ui'
 import type { RouteRecordRaw } from 'vue-router'
 import SideTitle from '@/layouts/side-menu/side-title.vue'
+import SideIcon from '@/layouts/side-menu/side-icon.vue'
 
 export const generateMenu = (routes: RouteRecordRaw[]) => {
   const menus: MenuOption[] = []
@@ -8,6 +9,7 @@ export const generateMenu = (routes: RouteRecordRaw[]) => {
     const currentMenu: MenuOption = {
       key: route.path,
       label: () => h(SideTitle, { route }),
+      icon: route.meta?.icon ? () => h(SideIcon, { icon: route.meta?.icon }) : undefined,
     }
 
     if (route.children && route.children.length > 0)
