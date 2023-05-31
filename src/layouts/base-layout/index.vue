d11f60bc5e28a0e1b35520c098fb1e331f7cef1d
adekang <adekang@163.com>
2023-05-31 11:18:45 +0800

    chore: 重构移动端布局

--- src/layouts/base-layout/index.vue
+++ src/layouts/base-layout/index.vue
@@ -1,56 +1,55 @@
 <script lang="ts" setup>
 import SettingDrawer from '../setting-drawer/index.vue'
+import RightContent from './right-content.vue'
 import WrapContent from './wrap-content.vue'
 import BasicLayout from './basic-layout.vue'
-import { useMenuState } from '@/compsables/menu-state'
-import { useUserStore } from '@/store/user'
-import RightContent from '@/layouts/base-layout/right-content.vue'
-import { useQueryBreakpoints } from '@/compsables/query-breakpoints'
 import { useAppstore } from '@/store/app'
-
+import { useUserStore } from '@/store/user'
+import { useQueryBreakpoints } from '@/compsables/query-breakpoints'
+import { useMenuState } from '@/compsables/menu-state'
 const appStore = useAppstore()
 const userStore = useUserStore()
 const menuOptions = computed(() => userStore.menusData)
-
 const { layout, visible, layoutList, layoutStyleList, themeList } =
   storeToRefs(appStore)
-
-const { isMobile, isDesktop, isPad } = useQueryBreakpoints()
-
+const { isMobile, isPad, isDesktop } = useQueryBreakpoints()
 const { active } = useMenuState()
-
 watchEffect(() => {
-  if (isDesktop.value) appStore.toggleCollapsed(false)
-  else if (isPad.value) appStore.toggleCollapsed(true)
-
-  if (isMobile.value) appStore.toggleCollapsed(false)
+  if (isPad.value) appStore.toggleCollapsed(true)
+  else if (isDesktop.value) appStore.toggleCollapsed(false)
+  if (isMobile.value) appStore.toggleVisible(false)
 })
 </script>
 
 <template>
   <BasicLayout
     v-model:collapsed="layout.collapsed"
-    :options="menuOptions"
+    v-model:visible="visible"
     :logo="layout.logo"
+    :title="layout.title"
+    :options="menuOptions"
+    :inverted="layout.layoutStyle === 'inverted'"
     :active="active"
     :layout="layout.layout"
-    :title="layout.title"
+    :is-mobile="isMobile"
     :show-sider-trigger="layout.showSiderTrigger"
     :sider-width="layout.siderWidth"
     :sider-collapsed-width="layout.siderCollapsedWidth"
-    :inverted="layout.layoutStyle === 'inverted'"
+    :header-fixed="layout.fixed"
   >
     <template #headerRight>
       <RightContent />
     </template>
     <WrapContent />
   </BasicLayout>
+
   <SettingDrawer
+    v-model:layout-style="layout.layoutStyle"
     v-model:layout="layout.layout"
     v-model:theme="layout.theme"
-    v-model:layout-style="layout.layoutStyle"
-    :theme-list="themeList"
+    v-model:header-fixed="layout.fixed"
     :layout-list="layoutList"
+    :theme-list="themeList"
     :layout-style-list="layoutStyleList"
   />
 </template>
